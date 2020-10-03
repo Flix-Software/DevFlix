6ea3b25597bf5db492b1ce6144647853fbe42d68
SahanDisa <t.sahan998@gmail.com>
2020-10-03 17:56:44 +0530

    mongoose model

--- DevFlix_API/server.js
+++ DevFlix_API/server.js
@@ -17,6 +17,21 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// database connection
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "It's DevFlix App." });
---
+++ DevFlix_API/src/config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    //change according to the database
+    url: "mongodb://localhost:27017/devflix_db"
+  };
