a1bd6ec1d96279d712b649e42d1ef9caca466e93
SahanDisa <t.sahan998@gmail.com>
2020-11-13 20:16:07 +0530

    Update Mongo+ Tutorial Component

--- DevFlix_API/server.js
+++ DevFlix_API/server.js
@@ -10,10 +10,25 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
+const db = require("./src/model");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+});
+
 const initRoutes = require("./src/routes");
 
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
+require("./src/routes/tutorial.route")(app);
 
 let port = 8080;
 app.listen(port, () => {
